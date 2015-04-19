13e7d0705b262baedd929377d4b4c3aed7aa5fd4
Bishakha <bishakha.mona@gmail.com>
2015-04-19 14:18:20 -0500

    instal dplyr package

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -3,27 +3,12 @@
 # bishakha.mona@gmail.com
 # April 19, 2015
 
+#Install dplyr package and dependencies - after done comment th ecommand in next line
+# install.packages("dplyr",dependencies = TRUE)
+
+# Load dplyr package into working environment
+library("dplyr")
+
 # Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
 
-# print first 10 rows of gap.in
-for(number in 1:10){
-  print(gap.in[number,])
-}
-
-# nest for loops
-for(row.number in 1:10){
-  for (col.number in 1:5){
-    print(gap.in[row.number,col.number])
-  }
-}
-
-gap.in[1:10,1:5]
-
-# create a function in R to add - set of code that does one thing
-add.me <- function(x,y){
-  x + y
-}
-
-# use the above add function
-add.me(3,4)
