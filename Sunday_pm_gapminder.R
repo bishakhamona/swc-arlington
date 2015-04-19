884e796f40d452c95e86b8dc8a13b1a9cae1b7a0
Bishakha <bishakha.mona@gmail.com>
2015-04-19 13:59:57 -0500

    playing with functions

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -5,3 +5,25 @@
 
 # Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t", header = TRUE)
+
+# print first 10 rows of gap.in
+for(number in 1:10){
+  print(gap.in[number,])
+}
+
+# nest for loops
+for(row.number in 1:10){
+  for (col.number in 1:5){
+    print(gap.in[row.number,col.number])
+  }
+}
+
+gap.in[1:10,1:5]
+
+# create a function in R to add - set of code that does one thing
+add.me <- function(x,y){
+  x + y
+}
+
+# use the above add function
+add.me(3,4)
